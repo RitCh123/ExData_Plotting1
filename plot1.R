41ab274c4a2707613143e2e06a856ad9a6d1921c
RitCh123 <ritchavali@gmail.com>
2021-06-23 20:42:16 -0700

    plot1.R is the code that describes the histogram that I created using the global active energy of the data set. The plot1.png is merely a PNG file of the histogram viewed on the computer.

---
+++ plot1.R
@@ -0,0 +1,5 @@
+homePower <- read.csv("~/Downloads/household_power_consumption.txt", sep=";", stringsAsFactors=FALSE)
+homePower$Date <- format(as.Date(homePower$Date, format = "%d/%m/%Y"), "%m/%d/%Y")
+
+globalPower <- homePower$Global_active_power[homePower$Date == "02/01/2007" | homePower$Date == "02/02/2007"]
+hist(as.numeric(globalPower), col = "red", xlab = "Global Active Power (kilowatts)", main = "Global Active Power")
