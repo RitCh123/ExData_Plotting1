62ddf3e46248a1acd4a2639ba9c383581aa8a56d
RitCh123 <ritchavali@gmail.com>
2021-06-24 13:56:41 -0700

    As per the isntructions, I had to show how the PNG file was created. That is added in both the plot1 and plot2.R files.

--- plot1.R
+++ plot1.R
@@ -2,4 +2,6 @@ homePower <- read.csv("~/Downloads/household_power_consumption.txt", sep=";", st
 homePower$Date <- format(as.Date(homePower$Date, format = "%d/%m/%Y"), "%m/%d/%Y")
 
 globalPower <- homePower$Global_active_power[homePower$Date == "02/01/2007" | homePower$Date == "02/02/2007"]
+png("plot1.png", width=480, height=480)
 hist(as.numeric(globalPower), col = "red", xlab = "Global Active Power (kilowatts)", main = "Global Active Power")
+dev.off()
